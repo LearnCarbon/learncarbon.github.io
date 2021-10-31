a5ac4015e9fe1893b764c0040162e1490222f7b3
anirudhbelwadi <anirudh.belwadi@gmail.com>
2021-10-31 13:33:53 +0400

    team and demo done

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -14,3 +14,31 @@ $(document).ready(function () {
         }
     });
 });
+
+$("#demo_video1").click(function () {
+    alert(1);
+    var video = $("#demo_video1").get(0);
+    if (video.paused) {
+        video.play();
+        $("#demo_video2").stop();
+        $("#demo_video2").stop();
+        $("#demo_video3").stop();
+        $("#demo_video4").stop();
+        $(".play").hide();
+        $(".pause").show();
+    } else {
+        video.pause();
+        $(".play").show();
+        $(".pause").hide();
+    }
+    return false;
+});
+
+function stop(a, b, c) {
+    var video1 = 'demo_video' + a;
+    var video2 = 'demo_video' + b;
+    var video3 = 'demo_video' + c;
+    $("#" + video1).get(0).pause();
+    $("#" + video2).get(0).pause();
+    $("#" + video3).get(0).pause();
+}
