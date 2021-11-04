d2c385fcbdb30b832b86568a93834a19b4c3949d
anirudhbelwadi <anirudh.belwadi@gmail.com>
2021-11-04 19:01:04 +0400

    code cleaned upand robots file added

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,6 +4,7 @@ demo_videos = document.getElementsByClassName('demo_video')
 for (j = 0; j < demo_videos.length; j++) {
     demo_videos[j].style.height = (9 / 16) * video_width + "px";
 }
+
 $(document).ready(function () {
     $(window).resize(function () {
         var j = 0;
